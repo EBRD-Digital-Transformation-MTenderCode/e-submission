2f136c06d9fb21446f3cdf830a8b7e2742b3f45c
Ruslan <ruslan.bur@gmail.com>
2017-12-07 16:41:37 +0200

    refactor controller advice

--- src/main/java/com/procurement/submission/controller/ControllerExceptionHandler.java
+++ src/main/java/com/procurement/submission/controller/ControllerExceptionHandler.java
@@ -6,43 +6,91 @@ import com.procurement.submission.exception.ValidationException;
 import com.procurement.submission.model.dto.response.ErrorInsertResponse;
 import com.procurement.submission.model.dto.response.MappingErrorResponse;
 import com.procurement.submission.model.dto.response.ValidationErrorResponse;
-import java.util.stream.Collectors;
-import org.springframework.http.HttpStatus;
+
+import java.util.function.Function;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.ServletException;
+import javax.validation.ConstraintViolationException;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @ControllerAdvice
 public class ControllerExceptionHandler {
-    private static final String MESSAGE = "Houston we have a problem";
+    private static final String MESSAGE = "Something went wrong";
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
-    public ValidationErrorResponse handleValidationContractProcessPeriod(
+    public ResponseEntity<ValidationErrorResponse> handleValidationContractProcessPeriod(
         final ValidationException e) {
+        return new ResponseEntity<>(getValidationErrorResponse(e.getErrors()), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> methodArgumentNotValidException(
+        final MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(getValidationErrorResponse(e.getBindingResult()), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ValidationErrorResponse> handle(
+        final ConstraintViolationException e) {
+        return new ResponseEntity<>(getValidationErrorResponse(e), BAD_REQUEST);
+    }
+
+    private ValidationErrorResponse getValidationErrorResponse(final ConstraintViolationException e) {
         return new ValidationErrorResponse(
             MESSAGE,
-            e.getErrors().getFieldErrors().stream()
-             .map(f -> new ValidationErrorResponse.ErrorPoint(
-                 f.getField(),
-                 f.getDefaultMessage(),
-                 f.getCode()))
-             .collect(Collectors.toList()));
+            e.getConstraintViolations()
+             .stream()
+             .map(s -> new ValidationErrorResponse.ErrorPoint(
+                 s.getPropertyPath().toString(),
+                 s.getMessage(),
+                 s.getMessageTemplate()))
+             .collect(toList()));
+    }
+
+    private ValidationErrorResponse getValidationErrorResponse(final BindingResult e) {
+        return new ValidationErrorResponse(
+            MESSAGE,
+            e.getFieldErrors()
+             .stream()
+             .map(getErrorPointFunction())
+             .collect(toList()));
+    }
+
+    private Function<FieldError, ValidationErrorResponse.ErrorPoint> getErrorPointFunction() {
+        return f -> new ValidationErrorResponse.ErrorPoint(
+            f.getField(),
+            f.getDefaultMessage(),
+            f.getCode());
     }
 
     @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(JsonMappingException.class)
     public MappingErrorResponse handleJsonMappingExceptionException(final JsonMappingException e) {
         return new MappingErrorResponse(MESSAGE, e);
     }
 
     @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(ErrorException.class)
     public ErrorInsertResponse handleErrorInsertException(final ErrorException e) {
+        return new ErrorInsertResponse(e.getMessage());
+    }
+
+    @ResponseBody
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(ServletException.class)
+    public ErrorInsertResponse handleErrorInsertException(final ServletException e) {
         return new ErrorInsertResponse(e.getMessage());
     }
 }
