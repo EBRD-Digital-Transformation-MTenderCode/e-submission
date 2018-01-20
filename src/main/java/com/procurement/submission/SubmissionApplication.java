e0659455cde76edf15625b4d3925916686dc981f
andreykolchev <andreykolchev@gmail.com>
2018-01-20 17:56:21 +0200

    add docker build config

--- src/main/java/com/procurement/submission/SubmissionApplication.java
+++ src/main/java/com/procurement/submission/SubmissionApplication.java
@@ -3,10 +3,12 @@ package com.procurement.submission;
 import com.procurement.submission.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(
     scanBasePackageClasses = ApplicationConfig.class
 )
+@EnableEurekaClient
 public class SubmissionApplication {
     public static void main(final String[] args) {
         SpringApplication.run(SubmissionApplication.class, args);
