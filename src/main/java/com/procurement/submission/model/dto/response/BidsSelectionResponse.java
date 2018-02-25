bd6f32969af9b94242c5c5dee6d061086d1a7949
BuildTools <unconfigured@null.spigotmc.org>
2018-02-25 23:56:56 +0200

    get bids refactor

--- src/main/java/com/procurement/submission/model/dto/response/BidsSelectionResponse.java
+++ src/main/java/com/procurement/submission/model/dto/response/BidsSelectionResponse.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.procurement.submission.databinding.LocalDateTimeSerializer;
 import com.procurement.submission.model.ocds.Value;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -48,27 +50,13 @@ public class BidsSelectionResponse {
     public static class Bid {
         private String id;
         private List<String> relatedLots;
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
         private LocalDateTime date;
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
         private LocalDateTime createDate;
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
         private LocalDateTime pendingDate;
         private Value value;
         private List<OrganizationReferenceRs> tenderers;
-
-        @JsonCreator
-        public Bid(@JsonProperty("id") final String id,
-                   @JsonProperty("relatedLots") final List<String> relatedLots,
-                   @JsonProperty("date") final LocalDateTime date,
-                   @JsonProperty("createDate") final LocalDateTime createDate,
-                   @JsonProperty("pendingDate") final LocalDateTime pendingDate,
-                   @JsonProperty("value") final Value value,
-                   @JsonProperty("tenderers") final List<OrganizationReferenceRs> tenderers) {
-            this.id = id;
-            this.relatedLots = relatedLots;
-            this.date = date;
-            this.createDate = createDate;
-            this.pendingDate = pendingDate;
-            this.value = value;
-            this.tenderers = tenderers;
-        }
     }
 }
