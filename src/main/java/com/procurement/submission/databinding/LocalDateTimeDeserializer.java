8c2ab4c5e7cde3730a31181ace35b13d12ae3ae6
andreykolchev <andreykolchev@gmail.com>
2018-03-05 17:04:28 +0200

    date format

--- src/main/java/com/procurement/submission/databinding/LocalDateTimeDeserializer.java
+++ src/main/java/com/procurement/submission/databinding/LocalDateTimeDeserializer.java
@@ -8,15 +8,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.temporal.ChronoField.*;
+
 public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
 
     private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
-        .parseCaseInsensitive()
-        .append(DateTimeFormatter.ISO_LOCAL_DATE)
-        .appendLiteral('T')
-        .append(DateTimeFormatter.ISO_LOCAL_TIME)
-        .appendLiteral('Z')
-        .toFormatter();
+            .parseCaseInsensitive()
+            .append(ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .appendLiteral('Z')
+            .toFormatter();
 
     protected LocalDateTimeDeserializer() {
         super(LocalDateTime.class);
--- src/main/java/com/procurement/submission/databinding/LocalDateTimeSerializer.java
+++ src/main/java/com/procurement/submission/databinding/LocalDateTimeSerializer.java
@@ -10,16 +10,24 @@ import java.time.format.DateTimeFormatterBuilder;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
 
 public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
 
     private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
-        .parseCaseInsensitive()
-        .append(ISO_LOCAL_DATE)
-        .appendLiteral('T')
-        .append(ISO_LOCAL_TIME)
-        .appendLiteral('Z')
-        .toFormatter();
+            .parseCaseInsensitive()
+            .append(ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .appendLiteral('Z')
+            .toFormatter();
 
     public LocalDateTimeSerializer() {
         super(LocalDateTime.class);
