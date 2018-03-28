5868ec439af8ae14259e3f7044308aedf5006649
andreykolchev <andreykolchev@gmail.com>
2018-03-28 14:06:45 +0300

    add deploy profile

--- src/main/java/com/procurement/submission/exception/ErrorType.java
+++ src/main/java/com/procurement/submission/exception/ErrorType.java
@@ -5,13 +5,13 @@ public enum ErrorType {
     BID_NOT_FOUND("00.01", "Bids not found."),
     INVALID_OWNER("00.02", "Invalid owner."),
     INVALID_ID("00.03", "Invalid bid id."),
-    PERIOD_NOT_FOUND("00.01", "Period not found."),
-    INVALID_DATE("00.03", "Date does not match the period."),
-    INVALID_PERIOD("00.03", "Invalid period."),
-    PERIOD_NOT_EXPIRED("00.03", "Period has not yet expired."),
-    INTERVAL_RULES_NOT_FOUND("00.03", "Interval rules not found."),
-    BIDS_RULES_NOT_FOUND("00.03", "Bids rules not found."),
-    BID_ALREADY_WITH_LOT("00.04", "We already have Bid with this Lots and Tenderers.");
+    PERIOD_NOT_FOUND("01.01", "Period not found."),
+    INVALID_PERIOD("01.02", "Invalid period."),
+    INVALID_DATE("01.03", "Date does not match the period."),
+    PERIOD_NOT_EXPIRED("01.04", "Period has not yet expired."),
+    INTERVAL_RULES_NOT_FOUND("02.01", "Interval rules not found."),
+    BIDS_RULES_NOT_FOUND("02.02", "Bids rules not found."),
+    BID_ALREADY_WITH_LOT("03.01", "We already have Bid with this Lots and Tenderers.");
 
     private final String code;
     private final String message;
