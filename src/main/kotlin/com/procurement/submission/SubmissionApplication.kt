573cdcbdce70ff32e2fd78a2e2aa766c1d3af290
andreykolchev <andreykolchev@gmail.com>
2018-09-21 16:41:24 +0300

    command

--- src/main/kotlin/com/procurement/submission/SubmissionApplication.kt
+++ src/main/kotlin/com/procurement/submission/SubmissionApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.submission
 import com.procurement.submission.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class SubmissionApplication
 
 fun main(args: Array<String>) {
