af983f96e57a5ca3153626443912f3569a6f46a0
andreykolchev <andreykolchev@gmail.com>
2018-10-01 17:51:51 +0300

    ES-1040 After 3 bids were sent to lot, lot became unsuccessful

--- src/main/kotlin/com/procurement/submission/model/dto/ocds/ContactPoint.kt
+++ src/main/kotlin/com/procurement/submission/model/dto/ocds/ContactPoint.kt
@@ -7,17 +7,13 @@ import javax.validation.constraints.NotNull
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class ContactPoint @JsonCreator constructor(
 
-        @field:NotNull
         val name: String,
 
-        @field:NotNull
         val email: String?,
 
-        @field:NotNull
         val telephone: String,
 
         val faxNumber: String?,
 
-        @field:NotNull
-        val url: String
+        val url: String?
 )
