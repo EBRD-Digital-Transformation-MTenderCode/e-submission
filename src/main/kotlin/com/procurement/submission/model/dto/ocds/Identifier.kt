a41de711324c1d60509ce1e51e4560a7534a333b
andreykolchev <andreykolchev@gmail.com>
2018-10-05 17:15:28 +0300

    identifier

--- src/main/kotlin/com/procurement/submission/model/dto/ocds/Identifier.kt
+++ src/main/kotlin/com/procurement/submission/model/dto/ocds/Identifier.kt
@@ -2,20 +2,15 @@ package com.procurement.submission.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
-import javax.validation.constraints.NotNull
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Identifier @JsonCreator constructor(
 
-        @field:NotNull
         val id: String,
 
-        @field:NotNull
         val scheme: String,
 
-        @field:NotNull
         val legalName: String,
 
-        @field:NotNull
-        val uri: String
+        val uri: String?
 )
