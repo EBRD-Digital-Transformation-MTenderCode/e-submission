bd2f1ab6fc85ca2a76b8409ff6cb53f5ba3c5855
andreykolchev <andreykolchev@gmail.com>
2018-10-19 13:52:06 +0300

    dev

---
+++ src/main/kotlin/com/procurement/submission/model/dto/response/BidDto.kt
@@ -0,0 +1,25 @@
+package com.procurement.submission.model.dto.response
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.procurement.submission.model.dto.ocds.OrganizationReference
+import com.procurement.submission.model.dto.ocds.Value
+import java.time.LocalDateTime
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class BidDto @JsonCreator constructor(
+
+        var id: String,
+
+        val date: LocalDateTime?,
+
+        var createDate: LocalDateTime,
+
+        var pendingDate: LocalDateTime?,
+
+        var value: Value?,
+
+        val tenderers: List<OrganizationReference>,
+
+        val relatedLots: List<String>
+)
