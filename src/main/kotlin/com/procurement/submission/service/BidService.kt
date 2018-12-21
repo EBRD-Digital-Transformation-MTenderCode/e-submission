c6027696c4a3c9495655054c175bc26189d3bd33
andreykolchev <andreykolchev@gmail.com>
2018-12-21 19:59:07 +0200

    update bid docs

--- src/main/kotlin/com/procurement/submission/service/BidService.kt
+++ src/main/kotlin/com/procurement/submission/service/BidService.kt
@@ -129,10 +129,9 @@ class BidService(private val generationService: GenerationService,
         val bid: Bid = toObject(Bid::class.java, entity.jsonData)
 
         //VR-4.8.4
-        //if (bid.status != Status.PENDING) throw ErrorException(INVALID_STATUSES_FOR_UPDATE)
-        //if (bid.statusDetails != StatusDetails.VALID) throw ErrorException(INVALID_STATUSES_FOR_UPDATE)
-        if (bid.status != Status.VALID) throw ErrorException(INVALID_STATUSES_FOR_UPDATE)
-        if (bid.statusDetails != StatusDetails.EMPTY) throw ErrorException(INVALID_STATUSES_FOR_UPDATE)
+        if ((bid.status != Status.PENDING && bid.statusDetails != StatusDetails.VALID)
+                && (bid.status != Status.VALID && bid.statusDetails != StatusDetails.EMPTY)
+        ) throw ErrorException(INVALID_STATUSES_FOR_UPDATE)
 
         //VR-4.8.5
         documentsDto.forEach { document ->
