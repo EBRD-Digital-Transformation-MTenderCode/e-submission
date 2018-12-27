4303dcce6bfbd13fe09a81e14fbbe358bb845a71
andreykolchev <andreykolchev@gmail.com>
2018-12-27 10:59:26 +0200

    check token

--- src/main/kotlin/com/procurement/submission/model/dto/response/BidsStatusRs.kt
+++ src/main/kotlin/com/procurement/submission/model/dto/response/BidsStatusRs.kt
@@ -2,10 +2,21 @@ package com.procurement.submission.model.dto.response
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.procurement.submission.model.dto.ocds.Bid
+import com.procurement.submission.model.dto.ocds.Status
+import com.procurement.submission.model.dto.ocds.StatusDetails
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class BidsStatusRs @JsonCreator constructor(
 
-        val bids: List<Bid>
+        val bids: List<FinalBid>
+)
+
+data class FinalBid @JsonCreator constructor(
+
+        var id: String,
+
+        var status: Status,
+
+        var statusDetails: StatusDetails
+
 )
