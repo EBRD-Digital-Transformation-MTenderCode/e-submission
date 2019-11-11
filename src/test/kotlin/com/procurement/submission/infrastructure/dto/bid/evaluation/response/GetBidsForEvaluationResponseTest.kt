6b91f4a97c3abb99f705f9c7fb602437ceeca366
Evgen Kopach <zkopach@gmail.com>
2019-11-11 14:25:31 +0200

    ES-2153 Created test for response dto at 'GetBidsForEvaluation' command

---
+++ src/test/kotlin/com/procurement/submission/infrastructure/dto/bid/evaluation/response/GetBidsForEvaluationResponseTest.kt
@@ -0,0 +1,29 @@
+package com.procurement.submission.infrastructure.dto.bid.evaluation.response
+
+import com.procurement.submission.infrastructure.AbstractDTOTestBase
+import com.procurement.submission.model.dto.response.BidCreateResponse
+import com.procurement.submission.model.dto.response.GetBidsForEvaludationResponse
+import org.junit.jupiter.api.Test
+
+class GetBidsForEvaluationResponseTest : AbstractDTOTestBase<GetBidsForEvaludationResponse>(GetBidsForEvaludationResponse::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/bid/evaluation/response/response_get_bids_for_evaluation_full.json")
+    }
+
+    @Test
+    fun required_1() {
+        testBindingAndMapping("json/infrastructure/dto/bid/evaluation/response/response_get_bids_for_evaluation_required_1.json")
+    }
+
+    @Test
+    fun required_2() {
+        testBindingAndMapping("json/infrastructure/dto/bid/evaluation/response/response_get_bids_for_evaluation_required_2.json")
+    }
+
+    @Test
+    fun required_3() {
+        testBindingAndMapping("json/infrastructure/dto/bid/evaluation/response/response_get_bids_for_evaluation_required_3.json")
+    }
+}
