545499d4f169e760036151b80abb827987909f11
Evgen Kopach <zkopach@gmail.com>
2019-11-22 15:15:42 +0200

    ES-2184 Created request dto for 'getBidsAuction' command

---
+++ src/main/kotlin/com/procurement/submission/model/dto/request/GetBidsAuctionRequest.kt
@@ -0,0 +1,13 @@
+package com.procurement.submission.model.dto.request
+
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.*
+
+data class GetBidsAuctionRequest(
+    @field:JsonProperty("lots") @param:JsonProperty("lots") val lots: List<Lot>
+) {
+    data class Lot(
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: UUID
+    )
+}
