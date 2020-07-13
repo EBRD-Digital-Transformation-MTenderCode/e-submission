dda28862f299ea023c13264e3cc4194420837e79
katherine <kate.shaidurova@gmail.com>
2020-07-13 16:15:16 +0300

    ES-3767 Added Stage.kt

---
+++ src/main/kotlin/com/procurement/submission/domain/model/enums/Stage.kt
@@ -0,0 +1,24 @@
+package com.procurement.submission.domain.model.enums
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class Stage(@JsonValue override val key: String) : EnumElementProvider.Key {
+
+    AC("AC"),
+    EI("EI"),
+    EV("EV"),
+    FS("FS"),
+    NP("NP"),
+    PN("PN"),
+    TP("TP");
+
+    override fun toString(): String = key
+
+    companion object : EnumElementProvider<Stage>(info = info()) {
+
+        @JvmStatic
+        @JsonCreator
+        fun creator(name: String) = orThrow(name)
+    }
+}
