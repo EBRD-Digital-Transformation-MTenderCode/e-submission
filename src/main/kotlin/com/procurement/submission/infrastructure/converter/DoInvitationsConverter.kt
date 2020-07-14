f2a358ace69e798136f154fa4c88180c66f34a13
katherine <kate.shaidurova@gmail.com>
2020-07-14 14:17:35 +0300

    ES-3758 Added DoInvitationsConverter.kt

---
+++ src/main/kotlin/com/procurement/submission/infrastructure/converter/DoInvitationsConverter.kt
@@ -0,0 +1,31 @@
+package com.procurement.submission.infrastructure.converter
+
+import com.procurement.submission.application.params.DoInvitationsParams
+import com.procurement.submission.domain.extension.mapResult
+import com.procurement.submission.infrastructure.dto.invitation.create.DoInvitationsRequest
+
+fun DoInvitationsRequest.convert() = DoInvitationsParams.tryCreate(
+    cpid = cpid,
+    date = date,
+    submissions = submissions.convert().orForwardFail { fail -> return fail },
+    qualifications = qualifications.mapResult { it.convert() }.orForwardFail { fail -> return fail }
+)
+
+fun DoInvitationsRequest.Qualification.convert() = DoInvitationsParams.Qualification.tryCreate(
+    id = id,
+    relatedSubmission = relatedSubmission,
+    statusDetails = statusDetails
+)
+
+fun DoInvitationsRequest.Submissions.convert() = DoInvitationsParams.Submissions.tryCreate(
+    details = details.mapResult { it.convert() }.orForwardFail { fail -> return fail }
+)
+
+fun DoInvitationsRequest.Submissions.Detail.convert() = DoInvitationsParams.Submissions.Detail.tryCreate(
+    id = id,
+    candidates = candidates.mapResult { it.convert() }.orForwardFail { fail -> return fail }
+)
+
+fun DoInvitationsRequest.Submissions.Detail.Candidate.convert() = DoInvitationsParams.Submissions.Detail.Candidate.tryCreate(
+    id = id, name = name
+)
