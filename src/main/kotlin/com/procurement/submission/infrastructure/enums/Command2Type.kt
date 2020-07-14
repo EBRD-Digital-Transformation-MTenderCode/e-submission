8d2be571292bd39c47de368266587896ae3719ad
katherine <kate.shaidurova@gmail.com>
2020-07-14 14:17:56 +0300

    ES-3758 Added doInvitations to Command2Type.kt and Command2Service.kt

--- src/main/kotlin/com/procurement/submission/infrastructure/enums/Command2Type.kt
+++ src/main/kotlin/com/procurement/submission/infrastructure/enums/Command2Type.kt
@@ -2,12 +2,12 @@ package com.procurement.submission.infrastructure.enums
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
-import com.procurement.submission.domain.model.enums.EnumElementProvider
 import com.procurement.submission.domain.Action
+import com.procurement.submission.domain.model.enums.EnumElementProvider
 
 enum class Command2Type(@JsonValue override val key: String) : Action, EnumElementProvider.Key {
 
-    TODO("todo");
+    DO_INVITATIONS("doInvitations");
 
     override fun toString(): String = key
 
