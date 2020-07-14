7af902c0ca49dcea0a3767107a8e88da025fb0cf
katherine <kate.shaidurova@gmail.com>
2020-07-14 17:49:48 +0300

    ES-3758 Added InvitationRepository.kt and its implementation

---
+++ src/main/kotlin/com/procurement/submission/application/repository/InvitationRepository.kt
@@ -0,0 +1,12 @@
+package com.procurement.submission.application.repository
+
+import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.functional.MaybeFail
+import com.procurement.submission.domain.functional.Result
+import com.procurement.submission.domain.model.Cpid
+import com.procurement.submission.domain.model.invitation.Invitation
+
+interface InvitationRepository {
+    fun findBy(cpid: Cpid): Result<List<Invitation>, Fail.Incident>
+    fun save(cpid: Cpid, invitation: Invitation): MaybeFail<Fail.Incident>
+}
