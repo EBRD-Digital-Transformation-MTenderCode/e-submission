e574466f3e895ed0d5d59b790fc7f6df9762ee8a
Evgen Kopach <zkopach@gmail.com>
2020-07-17 15:10:46 +0300

    ES-3781 Added error if active invitations was founded at 'checkAbsenceActiveInvitations' command

--- src/main/kotlin/com/procurement/submission/domain/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/submission/domain/fail/error/ValidationError.kt
@@ -1,6 +1,7 @@
 package com.procurement.submission.domain.fail.error
 
 import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.model.invitation.InvitationId
 import com.procurement.submission.domain.model.submission.SubmissionId
 
 sealed class ValidationError(
@@ -15,5 +16,12 @@ sealed class ValidationError(
     ) : ValidationError(
         numberError = "13.1.2",
         description = "Missing submission(s) by id(s) '${submissionIds.joinToString()}'."
+    )
+
+    class ActiveInvitationsFound(
+        invitations: Collection<InvitationId>
+    ) : ValidationError(
+        numberError = "13.2.1",
+        description = "Active invitations was found: invitation(s) by id(s): '${invitations.joinToString()}'."
     )
 }
