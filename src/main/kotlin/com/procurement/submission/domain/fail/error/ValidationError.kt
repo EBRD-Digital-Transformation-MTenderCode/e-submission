c1ddf2d69a74e8a02785a8d66b341651696c46cc
katherine <kate.shaidurova@gmail.com>
2020-07-17 13:49:54 +0300

    ES-3796 Added EntityNotFound and TenderPeriodDurationError to ValidationError.kt

--- src/main/kotlin/com/procurement/submission/domain/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/submission/domain/fail/error/ValidationError.kt
@@ -1,7 +1,10 @@
 package com.procurement.submission.domain.fail.error
 
 import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.ProcurementMethod
 import com.procurement.submission.domain.model.submission.SubmissionId
+import java.time.Duration
 
 sealed class ValidationError(
     numberError: String,
@@ -15,5 +18,20 @@ sealed class ValidationError(
     ) : ValidationError(
         numberError = "13.1.2",
         description = "Missing submission(s) by id(s) '${submissionIds.joinToString()}'."
+    )
+
+    sealed class EntityNotFound(description: String) : ValidationError("17", description) {
+
+        class TenderPeriodRule(
+            country: String,
+            pmd: ProcurementMethod,
+            parameter: String,
+            operationType: OperationType
+        ) : EntityNotFound("Tender period rule '$parameter' not found by country '$country', pmd '$pmd', operationType '$operationType'.")
+    }
+
+    class TenderPeriodDurationError(expectedDuration: Duration): ValidationError(
+        numberError = "1.17.2",
+        description = "Actual tender period duration is less than '${expectedDuration.toDays()}' days."
     )
 }
