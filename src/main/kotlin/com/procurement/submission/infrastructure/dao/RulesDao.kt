16892bc9b32fd35c29490c252bdadbbc07285689
katherine <kate.shaidurova@gmail.com>
2020-07-17 13:51:22 +0300

    ES-3796 Refactored getValue() in RulesDao.kt. Added tryGetValue() method

--- src/main/kotlin/com/procurement/submission/infrastructure/dao/RulesDao.kt
+++ src/main/kotlin/com/procurement/submission/infrastructure/dao/RulesDao.kt
@@ -1,31 +1,72 @@
 package com.procurement.submission.infrastructure.dao
 
 import com.datastax.driver.core.Session
-import com.datastax.driver.core.querybuilder.QueryBuilder.eq
-import com.datastax.driver.core.querybuilder.QueryBuilder.select
+import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.functional.Result
+import com.procurement.submission.domain.functional.asSuccess
+import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.ProcurementMethod
+import com.procurement.submission.infrastructure.extension.cassandra.tryExecute
 import org.springframework.stereotype.Service
 
 @Service
 class RulesDao(private val session: Session) {
 
-    fun getValue(country: String, pmd: String, parameter: String): String? {
-        val query = select()
-                .column(VALUE)
-                .from(RULES_TABLE)
-                .where(eq(CONTRY, country))
-                .and(eq(PMD, pmd))
-                .and(eq(PARAMETER, parameter))
-                .limit(1)
-        val row = session.execute(query).one()
-        return if (row != null) return row.getString(VALUE)
-        else null
+    companion object {
+
+        private const val RULES_TABLE = "submission_rules"
+        private const val KEYSPACE = "ocds"
+        private const val COUNTRY_COLUMN = "country"
+        private const val PMD_COLUMN = "pmd"
+        private const val OPERATION_TYPE_COLUMN = "operation_type"
+        private const val PARAMETER_COLUMN = "parameter"
+        private const val VALUE_COLUMN = "value"
+        private const val ALL_OPERATION_TYPE = "all"
+
+        private const val GET_VALUE_BY_CQL = """
+               SELECT $VALUE_COLUMN
+                 FROM $KEYSPACE.$RULES_TABLE
+                WHERE $COUNTRY_COLUMN=? 
+                  AND $PMD_COLUMN=?
+                  AND $OPERATION_TYPE_COLUMN=?
+                  AND $PARAMETER_COLUMN=?
+            """
     }
 
-    companion object {
-        private const val RULES_TABLE = "submission_rules"
-        private const val CONTRY = "country"
-        private const val PMD = "pmd"
-        private const val PARAMETER = "parameter"
-        private const val VALUE = "value"
+    private val preparedGetValueByCQL = session.prepare(GET_VALUE_BY_CQL)
+
+    fun getValue(country: String, pmd: String, parameter: String, operationType: String = ALL_OPERATION_TYPE): String? {
+        val query = preparedGetValueByCQL.bind()
+            .apply {
+                setString(COUNTRY_COLUMN, country)
+                setString(PMD_COLUMN, pmd)
+                setString(OPERATION_TYPE_COLUMN, operationType)
+                setString(PARAMETER_COLUMN, parameter)
+            }
+        return session
+            .execute(query)
+            .one()
+            ?.getString(VALUE_COLUMN)
+    }
+
+    fun tryGetValue(
+        country: String,
+        pmd: ProcurementMethod,
+        parameter: String,
+        operationType: OperationType? = null
+    ): Result<String?, Fail.Incident.Database.Interaction> {
+        val query = preparedGetValueByCQL.bind()
+            .apply {
+                setString(COUNTRY_COLUMN, country)
+                setString(PMD_COLUMN, pmd.name)
+                setString(OPERATION_TYPE_COLUMN, operationType?.key ?: ALL_OPERATION_TYPE)
+                setString(PARAMETER_COLUMN, parameter)
+            }
+
+        return query.tryExecute(session)
+            .orForwardFail { fail -> return fail }
+            .one()
+            ?.getString(VALUE_COLUMN)
+            .asSuccess()
     }
 }
