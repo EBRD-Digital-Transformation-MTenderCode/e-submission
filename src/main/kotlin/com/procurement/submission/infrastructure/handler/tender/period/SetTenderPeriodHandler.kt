8cab9d125dd1bb1fe66e16305c74925d9e8c9746
katherine <kate.shaidurova@gmail.com>
2020-07-17 18:08:38 +0300

    ES-3798 Added SetTenderPeriodHandler.kt

---
+++ src/main/kotlin/com/procurement/submission/infrastructure/handler/tender/period/SetTenderPeriodHandler.kt
@@ -0,0 +1,40 @@
+package com.procurement.submission.infrastructure.handler.tender.period
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.procurement.submission.application.repository.HistoryRepository
+import com.procurement.submission.application.service.Logger
+import com.procurement.submission.application.service.PeriodService
+import com.procurement.submission.application.service.Transform
+import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.functional.Result
+import com.procurement.submission.infrastructure.converter.convert
+import com.procurement.submission.infrastructure.dto.tender.period.set.SetTenderPeriodRequest
+import com.procurement.submission.infrastructure.dto.tender.period.set.SetTenderPeriodResult
+import com.procurement.submission.infrastructure.enums.Command2Type
+import com.procurement.submission.infrastructure.handler.AbstractHistoricalHandler2
+import com.procurement.submission.infrastructure.web.response.parser.tryGetParams
+import org.springframework.stereotype.Component
+
+@Component
+class SetTenderPeriodHandler(
+    logger: Logger,
+    historyRepository: HistoryRepository,
+    transform: Transform,
+    private val periodService: PeriodService
+) : AbstractHistoricalHandler2<Command2Type, SetTenderPeriodResult>(
+    logger = logger,
+    historyRepository = historyRepository,
+    target = SetTenderPeriodResult::class.java,
+    transform = transform
+) {
+    override val action: Command2Type = Command2Type.DO_INVITATIONS
+
+    override fun execute(node: JsonNode): Result<SetTenderPeriodResult, Fail> {
+        val params = node.tryGetParams(SetTenderPeriodRequest::class.java, transform = transform)
+            .orForwardFail { error -> return error }
+            .convert()
+            .orForwardFail { error -> return error }
+
+        return periodService.setTenderPeriod(params)
+    }
+}
