52e7ac052e4b98b9d5c7c618c6c5b18f4f89081e
katherine <kate.shaidurova@gmail.com>
2020-07-17 13:52:09 +0300

    ES-3796 Added getTenderPeriodMinimumDuration() to RulesService.kt

--- src/main/kotlin/com/procurement/submission/application/service/RulesService.kt
+++ src/main/kotlin/com/procurement/submission/application/service/RulesService.kt
@@ -2,39 +2,76 @@ package com.procurement.submission.application.service
 
 import com.procurement.submission.application.exception.ErrorException
 import com.procurement.submission.application.exception.ErrorType
+import com.procurement.submission.domain.extension.tryToLong
+import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.fail.error.ValidationError
+import com.procurement.submission.domain.functional.Result
+import com.procurement.submission.domain.functional.asFailure
+import com.procurement.submission.domain.functional.asSuccess
+import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.ProcurementMethod
 import com.procurement.submission.infrastructure.dao.RulesDao
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class RulesService(private val rulesDao: RulesDao) {
 
     fun getInterval(country: String, pmd: String): Long {
-        return rulesDao.getValue(country, pmd,
-                                 PARAMETER_INTERVAL
+        return rulesDao.getValue(
+            country, pmd,
+            PARAMETER_INTERVAL
         )?.toLongOrNull()
-                ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
+            ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
     }
 
     fun getUnsuspendInterval(country: String, pmd: String): Long {
-        return rulesDao.getValue(country, pmd,
-                                 PARAMETER_UNSUSPEND_INTERVAL
+        return rulesDao.getValue(
+            country, pmd,
+            PARAMETER_UNSUSPEND_INTERVAL
         )?.toLongOrNull()
-                ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
+            ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
     }
 
     fun getIntervalBefore(country: String, pmd: String): Long {
-        return rulesDao.getValue(country, pmd,
-                                 PARAMETER_INTERVAL_BEFORE
+        return rulesDao.getValue(
+            country, pmd,
+            PARAMETER_INTERVAL_BEFORE
         )?.toLongOrNull()
-                ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
+            ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
     }
 
-
     fun getRulesMinBids(country: String, pmd: String): Int {
-        return rulesDao.getValue(country, pmd,
-                                 PARAMETER_MIN_BIDS
+        return rulesDao.getValue(
+            country, pmd,
+            PARAMETER_MIN_BIDS
         )?.toIntOrNull()
-                ?: throw ErrorException(ErrorType.BIDS_RULES_NOT_FOUND)
+            ?: throw ErrorException(ErrorType.BIDS_RULES_NOT_FOUND)
+    }
+
+    fun getTenderPeriodMinimumDuration(
+        country: String,
+        pmd: ProcurementMethod,
+        operationType: OperationType
+    ): Result<Duration, Fail> {
+        val value = rulesDao
+            .tryGetValue(country, pmd, MINIMUM_PERIOD_DURATION_PARAMETER, operationType)
+            .orForwardFail { fail -> return fail }
+            ?: return ValidationError.EntityNotFound.TenderPeriodRule(
+                country = country,
+                pmd = pmd,
+                parameter = MINIMUM_PERIOD_DURATION_PARAMETER,
+                operationType = operationType
+            ).asFailure()
+
+        return value
+            .tryToLong()
+            .doReturn { incident ->
+                return Fail.Incident.Database.Parsing(VALUE_COLUMN, value, incident.exception)
+                    .asFailure()
+            }
+            .let { Duration.ofSeconds(it) }
+            .asSuccess()
     }
 
     companion object {
@@ -42,5 +79,8 @@ class RulesService(private val rulesDao: RulesDao) {
         private const val PARAMETER_INTERVAL = "interval"
         private const val PARAMETER_UNSUSPEND_INTERVAL = "unsuspend_interval"
         private const val PARAMETER_INTERVAL_BEFORE = "interval_before"
+        private const val MINIMUM_PERIOD_DURATION_PARAMETER = "minTenderPeriodDuration"
+
+        private const val VALUE_COLUMN = "value"
     }
 }
