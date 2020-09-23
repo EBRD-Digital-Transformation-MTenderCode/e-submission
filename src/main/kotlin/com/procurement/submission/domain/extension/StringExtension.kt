21a6a1daa4fff5910399ff2433100bc79c115771
katherine <kate.shaidurova@gmail.com>
2020-09-23 12:51:31 +0300

    ES-4303 Moved tryToLong to StringExtension.kt, added tryToBoolean()

--- src/main/kotlin/com/procurement/submission/domain/extension/StringExtension.kt
+++ src/main/kotlin/com/procurement/submission/domain/extension/StringExtension.kt
@@ -1,7 +1,9 @@
 package com.procurement.submission.domain.extension
 
-import com.procurement.submission.domain.functional.Result
 import com.procurement.submission.domain.fail.Fail
+import com.procurement.submission.domain.functional.Result
+import com.procurement.submission.domain.functional.asFailure
+import com.procurement.submission.domain.functional.asSuccess
 import java.util.*
 
 fun String.tryUUID(): Result<UUID, Fail.Incident.Transform.Parsing> =
@@ -12,3 +14,22 @@ fun String.tryUUID(): Result<UUID, Fail.Incident.Transform.Parsing> =
             Fail.Incident.Transform.Parsing(UUID::class.java.canonicalName, ex)
         )
     }
+
+fun String.tryToLong(): Result<Long, Fail.Incident.Transform.Parsing> = try {
+    this.toLong().asSuccess()
+} catch (exception: NumberFormatException) {
+    Fail.Incident.Transform.Parsing(className = Long::class.java.canonicalName, exception = exception).asFailure()
+}
+
+fun String.tryToBoolean(): Result<Boolean, Fail.Incident.Transform.Parsing> =
+    when {
+        this.equals("true", ignoreCase = true) -> true.asSuccess()
+        this.equals("false", ignoreCase = true) -> false.asSuccess()
+        else -> Fail.Incident.Transform.Parsing(className = Boolean::class.java.canonicalName).asFailure()
+    }
+
+
+fun main(){
+    val n = "test".toBoolean()
+    println(n)
+}
