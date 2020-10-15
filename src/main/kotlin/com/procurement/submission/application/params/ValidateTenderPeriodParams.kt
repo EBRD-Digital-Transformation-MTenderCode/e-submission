d4aeb55c3d202f8b46463e9a398365665aa700fa
katherine <kate.shaidurova@gmail.com>
2020-10-15 12:12:08 +0300

    ES-4462 Fixed createPcr when case

--- src/main/kotlin/com/procurement/submission/application/params/ValidateTenderPeriodParams.kt
+++ src/main/kotlin/com/procurement/submission/application/params/ValidateTenderPeriodParams.kt
@@ -45,8 +45,8 @@ class ValidateTenderPeriodParams private constructor(
         val allowedOperationTypes = OperationType.allowedElements
             .filter {
                 when (it) {
-                    OperationType.START_SECOND_STAGE -> true
                     OperationType.CREATE_PCR,
+                    OperationType.START_SECOND_STAGE -> true
                     OperationType.QUALIFICATION_PROTOCOL -> false
                 }
             }.toSet()
