b38e25dd60e071a0a1520b35dfe06d70b1643965
katherine <kate.shaidurova@gmail.com>
2020-10-26 13:34:26 +0200

    ES-4542 Added parsing methods

--- src/main/kotlin/com/procurement/submission/application/params/ParseParams.kt
+++ src/main/kotlin/com/procurement/submission/application/params/ParseParams.kt
@@ -1,6 +1,8 @@
 package com.procurement.submission.application.params
 
+import com.procurement.submission.domain.extension.UUID_PATTERN
 import com.procurement.submission.domain.extension.tryParseLocalDateTime
+import com.procurement.submission.domain.extension.tryUUID
 import com.procurement.submission.domain.fail.error.DataErrors
 import com.procurement.submission.domain.fail.error.DataTimeError
 import com.procurement.submission.domain.functional.Result
@@ -9,13 +11,23 @@ import com.procurement.submission.domain.functional.asSuccess
 import com.procurement.submission.domain.model.Cpid
 import com.procurement.submission.domain.model.Ocid
 import com.procurement.submission.domain.model.Owner
+import com.procurement.submission.domain.model.bid.BidId
+import com.procurement.submission.domain.model.enums.BusinessFunctionType
+import com.procurement.submission.domain.model.enums.DocumentType
 import com.procurement.submission.domain.model.enums.EnumElementProvider
 import com.procurement.submission.domain.model.enums.EnumElementProvider.Companion.keysAsStrings
 import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.PersonTitle
 import com.procurement.submission.domain.model.enums.ProcurementMethod
+import com.procurement.submission.domain.model.enums.ProcurementMethodModalities
 import com.procurement.submission.domain.model.enums.QualificationStatusDetails
+import com.procurement.submission.domain.model.enums.Scale
+import com.procurement.submission.domain.model.enums.TypeOfSupplier
+import com.procurement.submission.domain.model.item.ItemId
 import com.procurement.submission.domain.model.qualification.QualificationId
 import com.procurement.submission.domain.model.tryOwner
+import com.procurement.submission.model.dto.ocds.Amount
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 fun parseCpid(value: String): Result<Cpid, DataErrors.Validation.DataMismatchToPattern> =
@@ -73,13 +85,54 @@ fun parsePmd(
             )
         )
 
+fun parseDocumentType(
+    value: String,
+    allowedEnums: Set<DocumentType>,
+    attributeName: String
+): Result<DocumentType, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = DocumentType)
+
+fun parseTypeOfSupplier(
+    value: String,
+    allowedEnums: Set<TypeOfSupplier>,
+    attributeName: String
+): Result<TypeOfSupplier, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = TypeOfSupplier)
+
+fun parseScale(
+    value: String,
+    allowedEnums: Set<Scale>,
+    attributeName: String
+): Result<Scale, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = Scale)
+
+fun parsePersonTitle(
+    value: String,
+    allowedEnums: Set<PersonTitle>,
+    attributeName: String
+): Result<PersonTitle, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = PersonTitle)
+
+fun parseBusinessFunctionType(
+    value: String,
+    allowedEnums: Set<BusinessFunctionType>,
+    attributeName: String
+): Result<BusinessFunctionType, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = BusinessFunctionType)
+
+fun parseProcurementMethodModalities(
+    value: String,
+    allowedEnums: Set<ProcurementMethodModalities>,
+    attributeName: String
+): Result<ProcurementMethodModalities, DataErrors> =
+    parseEnum(value = value, allowedEnums = allowedEnums, attributeName = attributeName, target = ProcurementMethodModalities)
 
 private fun <T> parseEnum(
     value: String, allowedEnums: Set<T>, attributeName: String, target: EnumElementProvider<T>
 ): Result<T, DataErrors.Validation.UnknownValue> where T : Enum<T>,
                                                        T : EnumElementProvider.Key =
 
-     target.orNull(value)
+    target.orNull(value)
         ?.takeIf { it in allowedEnums }
         ?.asSuccess()
         ?: Result.failure(
@@ -94,7 +147,7 @@ fun parseDate(value: String, attributeName: String): Result<LocalDateTime, DataE
     value.tryParseLocalDateTime()
         .mapError { fail ->
             when (fail) {
-                is DataTimeError.InvalidFormat -> DataErrors.Validation.DataFormatMismatch(
+                is DataTimeError.InvalidFormat   -> DataErrors.Validation.DataFormatMismatch(
                     name = attributeName,
                     actualValue = value,
                     expectedFormat = fail.pattern
@@ -129,4 +182,43 @@ fun parseQualificationId(
         ).asFailure()
 
     return id.asSuccess()
+}
+
+fun parseBidId(
+    value: String, attributeName: String
+): Result<BidId, DataErrors.Validation.DataMismatchToPattern> {
+    val id = value.tryUUID().doReturn {
+        return DataErrors.Validation.DataMismatchToPattern(
+            name = attributeName,
+            pattern = UUID_PATTERN,
+            actualValue = value
+        ).asFailure()
+    }
+
+    return id.asSuccess()
+}
+
+fun parseItemId(
+    value: String, attributeName: String
+): Result<ItemId, DataErrors.Validation.DataMismatchToPattern> =
+    if (!ItemId.validate(value))
+        DataErrors.Validation.DataMismatchToPattern(
+            name = attributeName,
+            pattern = UUID_PATTERN,
+            actualValue = value
+        ).asFailure()
+    else ItemId.create(value).asSuccess()
+
+fun parseAmount(
+    value: BigDecimal, attributeName: String
+): Result<Amount, DataErrors.Validation.DataMismatchToPattern> {
+    val amount = Amount.tryCreate(value).doReturn { error ->
+        return DataErrors.Validation.DataMismatchToPattern(
+            name = attributeName,
+            pattern = error.description,
+            actualValue = value.toString()
+        ).asFailure()
+    }
+
+    return amount.asSuccess()
 }
