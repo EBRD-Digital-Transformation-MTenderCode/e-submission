341db2fc831f29709950ee1817e447416d57fa7f
katherine <kate.shaidurova@gmail.com>
2020-10-26 17:05:58 +0200

    ES-4535 Added request tests

---
+++ src/test/kotlin/com/procurement/submission/infrastructure/dto/bid/validate/ValidateBidDataRequestTest.kt
@@ -0,0 +1,28 @@
+package com.procurement.submission.infrastructure.dto.bid.validate
+
+import com.procurement.submission.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class ValidateBidDataRequestTest : AbstractDTOTestBase<ValidateBidDataRequest>(ValidateBidDataRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/infrastructure/dto/bid/validate/request_validate_bid_data_full.json")
+    }
+
+    @Test
+    fun required_1() {
+        testBindingAndMapping("json/infrastructure/dto/bid/validate/request_validate_bid_data_required.json")
+    }
+
+    @Test
+    fun required_2() {
+        testBindingAndMapping("json/infrastructure/dto/bid/validate/request_validate_bid_data_required_1.json")
+    }
+
+    @Test
+    fun required_3() {
+        testBindingAndMapping("json/infrastructure/dto/bid/validate/request_validate_bid_data_required_2.json")
+    }
+
+}
