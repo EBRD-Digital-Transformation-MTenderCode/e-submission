e4b89b33f7a65fd3a3c42a3c07873b52951c4a54
katherine <kate.shaidurova@gmail.com>
2020-10-27 14:37:22 +0200

    ES-4542 Added handler

--- src/main/kotlin/com/procurement/submission/infrastructure/service/Command2Service.kt
+++ src/main/kotlin/com/procurement/submission/infrastructure/service/Command2Service.kt
@@ -3,11 +3,12 @@ package com.procurement.submission.infrastructure.service
 import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.submission.application.service.Logger
 import com.procurement.submission.infrastructure.enums.Command2Type
+import com.procurement.submission.infrastructure.handler.bid.CreateBidHandler
 import com.procurement.submission.infrastructure.handler.invitation.CheckAbsenceActiveInvitationsHandler
 import com.procurement.submission.infrastructure.handler.invitation.DoInvitationsHandler
+import com.procurement.submission.infrastructure.handler.invitation.PublishInvitationsHandler
 import com.procurement.submission.infrastructure.handler.tender.period.SetTenderPeriodHandler
 import com.procurement.submission.infrastructure.handler.tender.period.ValidateTenderPeriodHandler
-import com.procurement.submission.infrastructure.handler.invitation.PublishInvitationsHandler
 import com.procurement.submission.infrastructure.web.api.response.ApiResponse2
 import com.procurement.submission.infrastructure.web.api.response.generator.ApiResponse2Generator.generateResponseOnFailure
 import com.procurement.submission.infrastructure.web.response.parser.tryGetAction
@@ -20,6 +21,7 @@ class Command2Service(
     private val logger: Logger,
     private val doInvitationsHandler: DoInvitationsHandler,
     private val checkAbsenceActiveInvitationsHandler: CheckAbsenceActiveInvitationsHandler,
+    private val createBidHandler: CreateBidHandler,
     private val validateTenderPeriodHandler: ValidateTenderPeriodHandler,
     private val setTenderPeriodHandler: SetTenderPeriodHandler,
     private val publishInvitationsHandler: PublishInvitationsHandler
@@ -47,6 +49,7 @@ class Command2Service(
         return when(action){
             Command2Type.DO_INVITATIONS -> doInvitationsHandler.handle(node)
             Command2Type.CHECK_ABSENCE_ACTIVE_INVITATIONS -> checkAbsenceActiveInvitationsHandler.handle(node)
+            Command2Type.CREATE_BID -> createBidHandler.handle(node)
             Command2Type.VALIDATE_TENDER_PERIOD -> validateTenderPeriodHandler.handle(node)
             Command2Type.SET_TENDER_PERIOD -> setTenderPeriodHandler.handle(node)
             Command2Type.PUBLISH_INVITATIONS -> publishInvitationsHandler.handle(node)
