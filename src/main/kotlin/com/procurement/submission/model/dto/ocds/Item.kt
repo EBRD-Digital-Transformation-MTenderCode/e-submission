7fe6fe624463236c865bedbaafacf51688e4831e
katherine <kate.shaidurova@gmail.com>
2020-10-27 18:00:17 +0200

    ES-4542 Added items to Bid

---
+++ src/main/kotlin/com/procurement/submission/model/dto/ocds/Item.kt
@@ -0,0 +1,16 @@
+package com.procurement.submission.model.dto.ocds
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.submission.domain.model.item.ItemId
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Item @JsonCreator constructor(
+    @param:JsonProperty("id") @field:JsonProperty("id") val id: ItemId,
+    @param:JsonProperty("unit") @field:JsonProperty("unit") val unit: Unit
+) {
+    data class Unit(
+        @param:JsonProperty("value") @field:JsonProperty("value") val value: Value
+    )
+}
