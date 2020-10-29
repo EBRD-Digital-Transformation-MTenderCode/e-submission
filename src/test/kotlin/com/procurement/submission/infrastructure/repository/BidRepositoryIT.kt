99046990255f79d5d79c999a7833cd2960667f1c
katherine <kate.shaidurova@gmail.com>
2020-10-29 17:26:08 +0200

    ES-4542 Fixed table name in tests

--- src/test/kotlin/com/procurement/submission/infrastructure/repository/BidRepositoryIT.kt
+++ src/test/kotlin/com/procurement/submission/infrastructure/repository/BidRepositoryIT.kt
@@ -61,7 +61,7 @@ class BidRepositoryIT {
         private val OCID = Ocid.tryCreateOrNull("ocds-b3wdp1-MD-1580458690892-EV-1580458791896")!!
 
         private const val KEYSPACE = "ocds"
-        private const val BID_TABLE = "submission_invitation"
+        private const val BID_TABLE = "submission_bid"
         private const val CPID_COLUMN = "cp_id"
         private const val STAGE_COLUMN = "stage"
         private const val BID_ID_COLUMN = "bid_id"
