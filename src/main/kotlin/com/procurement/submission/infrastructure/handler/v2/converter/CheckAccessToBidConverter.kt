efd85af53a993acea5cb4adb440ff60f396aaa23
katherine <kate.shaidurova@gmail.com>
2021-03-12 18:42:49 +0200

    ES-5731 Updated converter to check list

--- src/main/kotlin/com/procurement/submission/infrastructure/handler/v2/converter/CheckAccessToBidConverter.kt
+++ src/main/kotlin/com/procurement/submission/infrastructure/handler/v2/converter/CheckAccessToBidConverter.kt
@@ -6,10 +6,12 @@ import com.procurement.submission.application.params.parseCpid
 import com.procurement.submission.application.params.parseOcid
 import com.procurement.submission.application.params.parseOwner
 import com.procurement.submission.application.params.parseToken
+import com.procurement.submission.application.params.rules.notEmptyRule
 import com.procurement.submission.domain.fail.error.DataErrors
 import com.procurement.submission.infrastructure.handler.v2.model.request.CheckAccessToBidRequest
 import com.procurement.submission.lib.functional.Result
 import com.procurement.submission.lib.functional.asSuccess
+import com.procurement.submission.lib.functional.validate
 
 fun CheckAccessToBidRequest.convert(): Result<CheckAccessToBidParams, DataErrors> = CheckAccessToBidParams(
     cpid = parseCpid(cpid).onFailure { return it },
@@ -20,10 +22,13 @@ fun CheckAccessToBidRequest.convert(): Result<CheckAccessToBidParams, DataErrors
 ).asSuccess()
 
 fun CheckAccessToBidRequest.Bids.convert(): Result<CheckAccessToBidParams.Bids, DataErrors> = CheckAccessToBidParams.Bids(
-    details = details.map { detail ->
-        CheckAccessToBidParams.Bids.Detail(
-            id = parseBidId(detail.id, "bids.details.id").onFailure { return it }
-        )
+    details = details
+        .validate(notEmptyRule("bids.details"))
+        .onFailure { return it }
+        .map { detail ->
+            CheckAccessToBidParams.Bids.Detail(
+                id = parseBidId(detail.id, "bids.details.id").onFailure { return it }
+            )
     }
 ).asSuccess()
 
