b4b745739d1bbbbdfbdd3530fec534c7cd5e906d
katherine <kate.shaidurova@gmail.com>
2021-03-15 12:20:01 +0200

    ES-5733 Added dto and converter

---
+++ src/main/kotlin/com/procurement/submission/application/params/CheckBidStateParams.kt
@@ -0,0 +1,24 @@
+package com.procurement.submission.application.params
+
+import com.procurement.submission.domain.model.Cpid
+import com.procurement.submission.domain.model.Ocid
+import com.procurement.submission.domain.model.bid.BidId
+import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.ProcurementMethod
+
+data class CheckBidStateParams(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val bids: Bids,
+    val pmd: ProcurementMethod,
+    val country: String,
+    val operationType: OperationType
+) {
+    data class Bids(
+         val details: List<Detail>
+    ) {
+        data class Detail(
+             val id: BidId
+        )
+    }
+}
---
+++ src/main/kotlin/com/procurement/submission/infrastructure/handler/v2/converter/CheckBidStateConverter.kt
@@ -0,0 +1,65 @@
+package com.procurement.submission.infrastructure.handler.v2.converter
+
+import com.procurement.submission.application.params.CheckBidStateParams
+import com.procurement.submission.application.params.parseBidId
+import com.procurement.submission.application.params.parseCpid
+import com.procurement.submission.application.params.parseOcid
+import com.procurement.submission.application.params.parseOperationType
+import com.procurement.submission.application.params.parsePmd
+import com.procurement.submission.domain.fail.error.DataErrors
+import com.procurement.submission.domain.model.enums.OperationType
+import com.procurement.submission.domain.model.enums.ProcurementMethod
+import com.procurement.submission.infrastructure.handler.v2.model.request.CheckBidStateRequest
+import com.procurement.submission.lib.functional.Result
+import com.procurement.submission.lib.functional.asSuccess
+
+val allowedPmds = ProcurementMethod.values()
+    .filter {
+        when (it) {
+            ProcurementMethod.OT, ProcurementMethod.TEST_OT,
+            ProcurementMethod.SV, ProcurementMethod.TEST_SV,
+            ProcurementMethod.MV, ProcurementMethod.TEST_MV,
+            ProcurementMethod.GPA, ProcurementMethod.TEST_GPA,
+            ProcurementMethod.RT, ProcurementMethod.TEST_RT,
+            ProcurementMethod.CF, ProcurementMethod.TEST_CF,
+            ProcurementMethod.OF, ProcurementMethod.TEST_OF -> true
+
+            ProcurementMethod.CD, ProcurementMethod.TEST_CD,
+            ProcurementMethod.DA, ProcurementMethod.TEST_DA,
+            ProcurementMethod.DC, ProcurementMethod.TEST_DC,
+            ProcurementMethod.FA, ProcurementMethod.TEST_FA,
+            ProcurementMethod.IP, ProcurementMethod.TEST_IP,
+            ProcurementMethod.NP, ProcurementMethod.TEST_NP,
+            ProcurementMethod.OP, ProcurementMethod.TEST_OP -> false
+        }
+    }.toSet()
+
+val allowedOperationType = OperationType.values()
+    .filter {
+        when (it) {
+            OperationType.WITHDRAW_BID -> true
+            OperationType.COMPLETE_QUALIFICATION,
+            OperationType.CREATE_PCR,
+            OperationType.QUALIFICATION_PROTOCOL,
+            OperationType.START_SECOND_STAGE,
+            OperationType.SUBMIT_BID_IN_PCR -> false
+        }
+    }.toSet()
+
+fun CheckBidStateRequest.convert(): Result<CheckBidStateParams, DataErrors> = CheckBidStateParams(
+    cpid = parseCpid(cpid).onFailure { return it },
+    ocid = parseOcid(ocid).onFailure { return it },
+    pmd = parsePmd(pmd, allowedPmds).onFailure { return it },
+    country = country,
+    operationType = parseOperationType(operationType, allowedOperationType).onFailure { return it },
+    bids = bids.convert().onFailure { return it }
+).asSuccess()
+
+
+fun CheckBidStateRequest.Bids.convert(): Result<CheckBidStateParams.Bids, DataErrors> = CheckBidStateParams.Bids(
+    details = details.map { detail ->
+        CheckBidStateParams.Bids.Detail(
+            id = parseBidId(detail.id, "bids.details.id").onFailure { return it }
+        )
+    }
+).asSuccess()
