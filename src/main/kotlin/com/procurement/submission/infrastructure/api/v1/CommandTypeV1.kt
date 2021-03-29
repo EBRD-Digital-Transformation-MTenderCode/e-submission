351f219fec2082c09c00bf90c701ba7d4b55ac5b
Evgen Kopach <zkopach@gmail.com>
2021-03-29 11:27:21 +0300

    ES-5852 Removed 'updateBidBAwardStatus' command

--- src/main/kotlin/com/procurement/submission/infrastructure/api/v1/CommandTypeV1.kt
+++ src/main/kotlin/com/procurement/submission/infrastructure/api/v1/CommandTypeV1.kt
@@ -21,7 +21,6 @@ enum class CommandTypeV1(override val key: String) : Action {
     SAVE_NEW_PERIOD("saveNewPeriod"),
     SAVE_PERIOD("savePeriod"),
     UPDATE_BID("updateBid"),
-    UPDATE_BID_BY_AWARD_STATUS("updateBidBAwardStatus"),
     UPDATE_BID_DOCS("updateBidDocs"),
     VALIDATE_PERIOD("validatePeriod"),
     ;
